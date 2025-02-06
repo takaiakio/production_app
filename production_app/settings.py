6cbb0d6fdb25ddc69279fede3195dde8a028e4ef
takaiakio <akio.takai@alpsalpine.com>
2025-02-06 11:12:44 +0900

    host

--- production_app/settings.py
+++ production_app/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 import os
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.getenv("production-app-9kvg.onrender.com", 'localhost', "127.0.0.1").split(",")
 
 
 
